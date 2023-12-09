07196445aeed25741ada6cf465be83b998375024
HC12 <kasiamariuszszymon0@gmail.com>
2023-12-09 02:15:33 +0100

    deleted few files

--- api/user-info/devtools/generateToken.js
+++ api/user-info/devtools/generateToken.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const config = require('./config');
+const config = require('../../config');
 
 function generateToken(username, email, role) {
   const secretKey = config.getPrivateKey();
